de62f983c92a49220a1da6bd8a4dbe33e9ecfae2
Jana Platz <platzjana@gmail.com>
2025-03-17 14:01:32 +0100

    updated about component

--- home/src/app/about/about.component.ts
+++ home/src/app/about/about.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AboutService } from '../../services/about.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  aboutData: any = null;
 
+  constructor(private aboutService: AboutService) {}
+
+  async ngOnInit() {
+    try {
+      const data = await this.aboutService.getAboutData();
+      if (data) {
+        this.aboutData = {
+          ...data,
+          imageUrl: `http://127.0.0.1:8090/api/files/${data['collectionId']}/${data['id']}/${data['image']}`
+        };
+        console.log("Geladene Daten:", this.aboutData);
+      }
+    } catch (error) {
+      console.error("Fehler beim Laden der About-Daten:", error);
+    }
+  }
+  
 }
